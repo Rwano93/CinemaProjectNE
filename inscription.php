f876e1d010e8d5421afe63e8d57d865280464403
Nathan-Edward SONGHO-MALONGA <SONGHO-MALONGA_Na@lprs.local>
2023-12-08 09:42:02 +0100

    Modification des fichiers PHP

--- inscription.php
+++ inscription.php
@@ -1,20 +1,23 @@
 <?php
 // Connexion à la base de données
-$bdd = new PDO('mysql:host=localhost;dbname=erwan_site;charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=localhost;dbname=cinemaprojetc;charset=utf8', 'root', '');
 
 // Vérification si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupération des valeurs du formulaire
     $nom = $_POST["nom"];
     $prenom = $_POST["prenom"];
-    $age = $_POST["Age"]; // Si vous ne l'utilisez pas, vous pouvez le retirer
-    $metier = $_POST["metier"];
-    $pays = $_POST["Pays"];
+    $age = $_POST["age"]; // Si vous ne l'utilisez pas, vous pouvez le retirer
+    $pays = $_POST["pays"];
+    $ville = $_POST['ville'];
+    $rue = $_POST['rue'];
+    $cp = $_POST['cp'];
     $email = $_POST["mail"];
     $password = ($_POST["password"]);
+    $tel = $_POST["tel"];
 
     // Préparation et exécution de la requête SQL
-    $requete = $bdd->prepare("INSERT INTO user (nom, prenom, metier, Pays, email, mdp) VALUES (?, ?, ?, ?, ?, ?)");
+    $requete = $bdd->prepare("INSERT INTO user (nom, prenom, age, pays, ville, rue, cp, email, mdp, tel) VALUES (?,?,?,?,?,?,?,?,?,?)");
     $requete->execute([$nom, $prenom, $metier, $pays, $email, $password]);
 
     // Vérification de l'insertion
